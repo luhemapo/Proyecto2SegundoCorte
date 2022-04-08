a49158c648c66fcadf6c5544b8ba511b5418abe4
Luhemapo <luhemapo@hotmail.com>
2022-04-08 13:32:54 -0500

    method deleted

--- Multiplicacion&Matriz/src/Controller/Controller.java
+++ Multiplicacion&Matriz/src/Controller/Controller.java
@@ -16,11 +16,11 @@ public class Controller implements ActionListener {
 	public String input2 = "";
 	public int n = 0;
 	public BigInteger result = new BigInteger("0");
+
 	public Controller() {
 		viewM = new ViewMain();
 		viewMult = new ViewMult();
 		mult = new Multiplication();
-		
 
 		viewM.getBt_mult().addActionListener(this);
 		viewM.getBt_matriz().addActionListener(this);
@@ -29,28 +29,20 @@ public class Controller implements ActionListener {
 		viewMult.getBt_atras().addActionListener(this);
 
 	}
+
 	public BigInteger runMultiply() {
 		String in1 = viewMult.getTa_num1().getText();
 		String in2 = viewMult.getTa_num2().getText();
 		BigInteger input1 = new BigInteger(in1);
 		BigInteger input2 = new BigInteger(in2);
 		n = Math.max(input1.bitLength(), input2.bitLength());
-		if(n < 6) {
+		if (n < 6) {
 			result = mult.smallMulti(input1, input2);
-		}else {
+		} else {
 			result = mult.multi(input1, input2, n);
-			
+
 		}
 		return result;
-		
-	}
-	public static boolean lengthEven(String input) {
-		int even = input.length() % 2;
-		if (even == 1) {
-			return false;
-		} else {
-			return true;
-		}
 
 	}
 
@@ -69,24 +61,23 @@ public class Controller implements ActionListener {
 			viewM.setVisible(true);
 		}
 		if (e.getActionCommand().equals("MULT_MULTIPLICAR")) {
-			if(!viewMult.getTa_num1().getText().equals("")&&!viewMult.getTa_num2().getText().equals("")) {
-				if(mult.isNumeric(viewMult.getTa_num1().getText())== false ||mult.isNumeric(viewMult.getTa_num2().getText())== false) {
-					viewM.showMessage("Input no valido", "Error");
-				}else {
-					if(Integer.parseInt(viewMult.getTa_num1().getText())>0 && Integer.parseInt(viewMult.getTa_num2().getText())>0) {
+			if (!viewMult.getTa_num1().getText().equals("") && !viewMult.getTa_num2().getText().equals("")) {
+				if (mult.isNumeric(viewMult.getTa_num1().getText()) == false
+						|| mult.isNumeric(viewMult.getTa_num2().getText()) == false) {
+					viewM.showMessage("Input no valido, ingrese numeros unicamente", "Error");
+				} else {
+					if (Integer.parseInt(viewMult.getTa_num1().getText()) > 0
+							&& Integer.parseInt(viewMult.getTa_num2().getText()) > 0) {
 						runMultiply();
 						viewMult.getTa_result().setText(result.toString());
-					}else {
+					} else {
 						viewM.showMessage("Debe ingresar 2 numeros positivos", "Error");
 					}
 				}
-					
-				
-			}else {
+
+			} else {
 				viewM.showMessage("Debe ingresar 2 numeros positivos", "Error");
 			}
-			
-			
 		}
 	}
 }

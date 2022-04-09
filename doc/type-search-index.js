484ee4b04bb8fca380380f7f8373467cafd000e8
Luhemapo <luhemapo@hotmail.com>
2022-04-09 16:04:36 -0500

    gg

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Controller","l":"Controller"},{"p":"Controller","l":"Main"},{"p":"Model","l":"Matrices"},{"p":"Model","l":"Multiplication"},{"p":"View","l":"ViewMain"},{"p":"View","l":"ViewMat"},{"p":"View","l":"ViewMult"}];updateSearchResults();
